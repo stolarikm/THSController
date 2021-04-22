ed35dc8f6d90bfc62656855f9c0efb9ff9284613
STOLÁRIK Michal <michal.stolarik@asseco-ce.com>
2021-04-22 19:10:22 +0200

    NetworkScanServiceTest, NetworkScanService fix

---
+++ __mocks__/react-native-get-subnet-mask.js
@@ -0,0 +1,4 @@
+export default {
+    getIpV4: (fn) => fn("192.168.100.57"),
+    getSubnet: (fn) => fn("255.255.255.0")
+}
--- services/NetworkScanService.js
+++ services/NetworkScanService.js
@@ -60,7 +60,7 @@ export default class NetworkScanService {
     }
   }
 
-  static async stop() {
+  static stop() {
     NetworkScanService.shouldStop = true;
   }
 }
